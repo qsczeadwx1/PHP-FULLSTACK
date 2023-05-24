18b758cd99c705daef20ead28e17f75a06516394
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-05-24 17:52:31 +0900

    0524

--- edu-laravel/database/seeders/DatabaseSeeder.php
+++ edu-laravel/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\CategorySeeder; // 시더 use
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // 초기 데이터 삽입용 시더 호출
+        // $this->call(CategorySeeder::class);
+
         // \App\Models\User::factory(10)->create();
+
+        // 더미 데이터  삽입용 팩토리 호출
+        \App\Models\Board::factory(100)->create();
+        \App\Models\Category::factory(100)->create();
     }
 }
+
+
