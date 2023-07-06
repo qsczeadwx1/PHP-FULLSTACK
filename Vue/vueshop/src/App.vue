14c5978b358eb7551278379241c96c311bcaf105
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-07-06 17:37:19 +0900

    0706 vue start

---
+++ Vue/vueshop/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <div v-for="(item, i) in products" :key="i">
+        <h4>{{ item.name }}</h4>
+        <p>{{ item.price }}</p>
+  </div>
+
+  <!-- <div>
+    <h4 :style="styleR">{{ product1 }}</h4>
+    <p>{{ price1 }}원</p>
+  </div>
+  <div>
+    <h4>{{ product2 }}</h4>
+    <p>{{ price2 }}원</p>
+  </div> -->
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() { // 데이터 바인딩
+    return {
+      products: [
+        {name: '티셔츠', price: '3800'}
+        ,{name: '바지', price: '5000'}
+        ,{name: '점퍼', price: '10000'}
+      ],
+      product1: '장갑',
+      price1: '30,000',
+      product2: '바지',
+      price2: '50,000',
+      styleR: 'color:blue',
+      styleL: 'list-style: none'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
